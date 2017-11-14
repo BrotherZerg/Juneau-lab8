7cd5c6f16e57b158256ca8663ddd2d3412a59dce
PJennyP <32084727+PJennyP@users.noreply.github.com>
2017-11-14 12:37:16 -0500

    Add files via upload

---
+++ Assingment 8/survey-thanks.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>CSC 174 | Team Juneau</title>
+    
+    
+  </head>
+<body>
+		<h1>Thank you for taking your time to give us your feedback!</h1>
+		<a href="survey.html">Back</a>
+</body>
+</html>
