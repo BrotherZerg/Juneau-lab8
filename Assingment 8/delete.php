da7834abd39890fdf3a862715544fc4fd6e0501a
BrotherZerg <32111274+BrotherZerg@users.noreply.github.com>
2017-11-14 16:59:55 -0500

    Update delete.php

--- Assingment 8/delete.php
+++ Assingment 8/delete.php
@@ -2,7 +2,7 @@
 // Create a database connection
 $mysqli = new mysqli('66.147.242.186', 'urcscon3_juneau', 'coffee1N', 'urcscon3_juneau');
 
-$query  = "DELETE FROM survey ";
+$query  = "DELETE FROM 'survey' ";
 $query .= "WHERE ID = {$_POST['number']} ";
 $query .= "LIMIT 1";
 $result = mysqli_query($mysqli, $query);
