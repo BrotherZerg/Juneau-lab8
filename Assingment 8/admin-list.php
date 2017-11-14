f0112489c3ec73b90350f591a27a256cf8a8a6df
zjiang0820 <31747168+zjiang0820@users.noreply.github.com>
2017-11-14 16:53:49 -0500

    Update admin-list.php

--- Assingment 8/admin-list.php
+++ Assingment 8/admin-list.php
@@ -3,44 +3,39 @@
   $mysqli = new mysqli('66.147.242.186', 'urcscon3_juneau', 'coffee1N', 'urcscon3_juneau');
 
   $sql = "SELECT * FROM `survey`";
-  
-  $number = $_POST['number'];
-  $query .= "WHERE ID = {$number}";
 
-  $result = mysqli_query($mysqli,$query);
+  
+  $result = mysqli_query($mysqli,$sql);
 ?>
 
-
-<!DOCTYPE html>
-<html>
-<head>
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
   <title>Assignment 8</title>
-</head>
-<body>
-  <h1 align = "center"><?php echo $row["Name"]; ?>'s Survey Result</h1>
-  <table>
-<?php
+  </head>
+  
+  <body>
+      <table border="1" align="center" style="line-height: 25px;">
 
-  while($row = mysqli_fetch_assoc($result)){
+  <?php
+      while($row = mysqli_fetch_assoc($result)){
     ?>
     <tr><td>ID:</td><td><?php echo $row["ID"]; ?></td></tr>
     <tr><td>Name:</td><td><?php echo $row["Name"]; ?></td></tr>
     <tr><td>Email:</td><td><?php echo $row["Email"]; ?></td></tr>
     <tr><td>Telephone:</td><td><?php echo $row["Telephone"]; ?></td></tr>
     <tr><td>Campus Status:</td><td><?php echo $row["CampusStatus"]; ?></td></tr>
-    <tr><td>Food Location:</td><td><?php echo $row["FoodLation1"]; ?></td><td><?php echo $row["FoodLation2"]; ?></td><td><?php echo $row["FoodLation3"]; ?></td><td><?php echo $row["FoodLation4"]; ?></td><td><?php echo $row["FoodLation5"]; ?></td></tr>
+    <tr><td>Food Location:</td><td><?php echo $row["FoodLocation1"]; ?></td><td><?php echo $row["FoodLocation2"]; ?></td><td><?php echo $row["FoodLocation3"]; ?></td><td><?php echo $row["FoodLocation4"]; ?></td><td><?php echo $row["FoodLocation5"]; ?></td></tr>
     <tr><td>Quality Rate:</td><td><?php echo $row["QualityRate"]; ?></td></tr>
     <tr><td>Food Purchase:</td><td><?php echo $row["FoodPurchase"]; ?></td></tr>
     <tr><td>Where To Get Food:</td><td><?php echo $row["WhereToGetFood"]; ?></td></tr>
     <tr><td>Dietary Needs:</td><td><?php echo $row["DietaryNeeds"]; ?></td></tr>
     <tr><td>Special Dietary:</td><td><?php echo $row["SpecialDietary"]; ?></td></tr>
     <tr><td>Cafeteria:</td><td><?php echo $row["Cafeteria"]; ?></td></tr>
-    <tr><td>Message:</td><td><?php echo $row["Message"]; ?></td></tr>   
-   
-    
+    <tr><td>Message:</td><td><?php echo $row["Message"]; ?></td></tr> 
+      
   <?php } ?>
-  
-  </table>
+ </table>
   <br>
   <form method= "post" action= "delete.php">
     <a href="delete.php">Delete</a>
@@ -60,8 +55,4 @@
 ?>
 <br>
 <a href="admin.php">Back</a>
-
-
-
-
 
