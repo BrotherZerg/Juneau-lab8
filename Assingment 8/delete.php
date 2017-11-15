16c71eafa120517541512e3c5e42763301ed18a1
zjiang0820 <31747168+zjiang0820@users.noreply.github.com>
2017-11-15 01:07:10 -0500

    Update delete.php

--- Assingment 8/delete.php
+++ Assingment 8/delete.php
@@ -2,21 +2,44 @@
 // Create a database connection
 $mysqli = new mysqli('66.147.242.186', 'urcscon3_juneau', 'coffee1N', 'urcscon3_juneau');
 
-$query  = "DELETE FROM 'survey' ";
-$query .= "WHERE ID = {$_POST['number']} ";
+$id = $_POST ['id'];
+
+
+$query = "DELETE FROM survey ";
+$query .= "WHERE id = {$id} ";
 $query .= "LIMIT 1";
+
 $result = mysqli_query($mysqli, $query);
 ?>
 
-<?php
-if ($result) {
-?>
-    <div>
-        The record <?php echo $_POST['number'] ?> has been deleted.
-    </div>
+<!doctype html>
+<html class="no-js" lang="en">
+	<head>
+		<title>Assignment 8 </title>
+	</head>
+		<body>
+		<?php
+			if ($result) {
+		?>
+    		<div>
+    		<h2 align = "center" >The record has been deleted!</h2>	
+    		</div>
+
+		<?php
+			} else {
+    			die("Database query failed.");
+			}
+		?>
+
+
+		</body>
+</html>
+
+
 
 <?php
-} else {
-    die("Database query failed.");
-}
+	mysqli_close($connection);
 ?>
+
+<br>
+<a href="admin.php">Back</a>
