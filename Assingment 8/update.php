71fb520eacc7c17ee7f05b1131d12e1cf39446c9
zjiang0820 <31747168+zjiang0820@users.noreply.github.com>
2017-11-15 17:17:37 -0500

    Update update.php

--- Assingment 8/update.php
+++ Assingment 8/update.php
@@ -8,11 +8,56 @@ $updatedName = Trim(stripslashes($_POST['name']));
 $updatedEmail =  Trim(stripslashes($_POST['email']));
 $updatedTelephone =  Trim(stripslashes($_POST['telephone']));
 
+//question1
+$statusselect=$_POST["status"];
+
+//question2
+$location1=$_POST["location1"];
+$location2=$_POST["location2"];
+$location3=$_POST["location3"];
+$location4=$_POST["location4"];
+$location5=$_POST["location5"];
+
+
+//question3
+$qualityselect=$_POST["quality"];
+
+//question4
+$purchaseselect=$_POST["purchase"];
+
+//question5
+$eatselect=$_POST["eat"];
+
+//question6
+$dietaryselect=$_POST["dietary"];
+
+//question7
+$specialdietaryselect=$_POST["special-dietary"];
+
+//question8
+$cafeteriaselect=$_POST["cafeteria"];
+
+$message=trim(stripslashes($_POST["message"]));
 
 
 
 $query  = "UPDATE survey SET ";
-$query .= "Name = '$updatedName', Email = '$updatedEmail',Telephone = '$updatedTelephone'";
+$query .= "Name = '$updatedName',";
+$query .="Email = '$updatedEmail',";
+$query .="Telephone = '$updatedTelephone', ";
+$query .="CampusStatus = '$statusselect', ";
+$query .="FoodLocation1 = '$location1', ";
+$query .="FoodLocation2 = '$location2',";
+$query .="FoodLocation3 = '$location3',";
+$query .="FoodLocation4 = '$location4',";
+$query .="FoodLocation5 = '$location5', ";
+$query .="QualityRate = '$qualityselect', ";
+$query .="FoodPurchase = '$purchaseselect', ";
+$query .="WhereToGetFood = '$eatselect', ";
+$query .="DietaryNeeds = '$dietaryselect', ";
+$query .="SpecialDietary = '$specialdietaryselect', ";
+$query .="Cafeteria = '$cafeteriaselect', ";
+$query .="Message = '$message'";
 $query .= "WHERE ID = {$number}";
 $result = mysqli_query($mysqli, $query);
 
