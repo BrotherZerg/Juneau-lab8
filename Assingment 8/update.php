659cb0324b4efc5b72c854f070c9e556f124148e
zjiang0820 <31747168+zjiang0820@users.noreply.github.com>
2017-11-15 12:30:35 -0500

    Update update.php

--- Assingment 8/update.php
+++ Assingment 8/update.php
@@ -3,13 +3,17 @@
 // Create a database connection
 $mysqli = new mysqli('66.147.242.186', 'urcscon3_juneau', 'coffee1N', 'urcscon3_juneau');
 
-$number = $_POST['number'];
+$number = $_POST['id'];
 $updatedName = Trim(stripslashes($_POST['name']));
 $updatedEmail =  Trim(stripslashes($_POST['email']));
+$updatedTelephone =  Trim(stripslashes($_POST['telephone']));
+
+
+
 
 $query  = "UPDATE survey SET ";
-$query .= "Name = '$updatedName', Email = '$updatedEmail' ";
-$query .= "WHERE ID = '{$number}' ";
+$query .= "Name = '$updatedName', Email = '$updatedEmail',Telephone = '$updatedTelephone'";
+$query .= "WHERE ID = {$number}";
 $result = mysqli_query($mysqli, $query);
 
 
@@ -27,3 +31,12 @@ if ($result) {
     die("Database query failed.");
 }
 ?>
+
+<?php
+mysqli_close($connection);
+?>
+
+<br>
+<a href="admin.php">Back</a>
+
+
